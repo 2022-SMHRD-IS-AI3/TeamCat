c97c385589f0545d372c11591e2cf38668678b89
SMHRD <SMHRD@59.0.234.4>
2023-03-29 20:08:37 +0900

    주연-관심내역완료

--- Kkeonaeng/src/main/java/com/smhrd/model/WishlistDAO.java
+++ Kkeonaeng/src/main/java/com/smhrd/model/WishlistDAO.java
@@ -8,35 +8,45 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import com.smhrd.mybatis.SqlSessionManager;
 
 public class WishlistDAO {
-	
+
 	SqlSessionFactory sqlSessionfactory = SqlSessionManager.getSqlSession();
 
 	// 위시리스트 업로드
 	public int WishlistUpdate(WishlistDTO dto) {
 		System.out.println("dao");
 		SqlSession sqlsession = sqlSessionfactory.openSession(true);
-		int cnt = sqlsession.insert("WishlistUpdate", dto);	
+		int cnt = sqlsession.insert("WishlistUpdate", dto);
 		sqlsession.close();
-		 		
+
 		return cnt;
 	}
-		
+
 	// 위시리스트 삭제
 	public int WishlistDelete(WishlistDTO dto) {
 		System.out.println(123);
 		SqlSession sqlsession = sqlSessionfactory.openSession(true);
 		int cnt = sqlsession.delete("WishlistDelete", dto);
 		sqlsession.close();
-			
+
 		return cnt;
 	}
-		
-	// 게시글 목록 조회
+
+	// 위시 목록 조회
 	public List<WishlistDTO> WishlistAll() {
 		SqlSession sqlsession = sqlSessionfactory.openSession(true);
 		List<WishlistDTO> Wishlist_list = sqlsession.selectList("WishlistAll");
 		sqlsession.close();
-			
+
 		return Wishlist_list;
 	}
+
+	// 개인 관심목록
+	public List<ProductDTO> myWishlist(int user_idx) {
+		SqlSession sqlsession = sqlSessionfactory.openSession(true);
+		List<ProductDTO> Product_list = sqlsession.selectList("myWishlist", user_idx);
+		sqlsession.close();
+
+		return Product_list;
+	}
+
 }
