1bfa63ffc39c2bf4ff2b26e383000edcb522f89d
SMHRD <SMHRD@222.102.104.91>
2023-03-30 16:02:00 +0900

    admin(리뷰 삭제 중)

--- Kkeonaeng/src/main/java/com/smhrd/controller/ReviewDeleteCon.java
+++ Kkeonaeng/src/main/java/com/smhrd/controller/ReviewDeleteCon.java
@@ -2,6 +2,7 @@ package com.smhrd.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.smhrd.Command.Command;
 import com.smhrd.model.ReviewDAO;
@@ -9,23 +10,30 @@ import com.smhrd.model.ReviewDTO;
 
 public class ReviewDeleteCon implements Command {
 
-public String execute(HttpServletRequest request, HttpServletResponse response) {
-		
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+
+		System.out.println("111");
 		int rv_idx = Integer.parseInt(request.getParameter("rv_idx"));
-		//System.out.println(rv_idx);
+		System.out.println("222");
+		String admin = request.getParameter("admin");
+		System.out.println("333");
 		ReviewDAO dao = new ReviewDAO();
+		System.out.println("444");
 		int cnt = dao.ReviewDelete(rv_idx);
-		
+		String moveUrl = "";
 		if (cnt > 0) {
-			System.out.println("삭제성공"); // 지울예정
-			
+			System.out.println("삭제 성공"); // 지울예정
+			moveUrl = "../";
+			if (admin != null) {
+				moveUrl = "/Kkeonaeng/Admin/Review/";
+			}
+
 		} else {
-			System.out.println("삭제실패"); // 지울예정
+			System.out.println("삭제 실패"); // 지울예정
+
 		}
-		
-		return "../"; 
+
+		return "../";
 	}
 
 }
-
-
